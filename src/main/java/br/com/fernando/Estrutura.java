d4221ba015ff99bd1c15024bf6f7bd2acea59b5c
Fernando Pimenta <fernando.pimenta107@gmail.com>
2019-06-14 19:06:05 -0300

    Vector

---
+++ src/main/java/br/com/fernando/Estrutura.java
@@ -0,0 +1,4 @@
+package br.com.fernando;
+
+public class Estrutura {
+}
---
+++ src/main/java/br/com/fernando/vector/VectorImpl.java
@@ -0,0 +1,23 @@
+package br.com.fernando.vector;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
+
+public class VectorImpl {
+
+    public static void main(String[] args) {
+
+        Vector vector = new Vector();
+
+        vector.add("Felipe");
+        vector.add(88.8);
+        vector.add(new ArrayList<>());
+
+        Iterator iterator = vector.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+    }
+}
